dd619b9f575277167a336a54de53d3e48b41aae3
Miloch <michal.miloch@gmail.com>
2019-08-10 11:50:51 +0200

    Updates

---
+++ precache-manifest.97a96432ebbd5bb074dd280a45ef1709.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd22c51e8a8a125e673935ef8768a07",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "639df8843ba65cf8e878",
+    "url": "/react_app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2697f33228be243d6f36",
+    "url": "/react_app/static/js/2.8efbad62.chunk.js"
+  },
+  {
+    "revision": "639df8843ba65cf8e878",
+    "url": "/react_app/static/js/main.a97f2a1b.chunk.js"
+  },
+  {
+    "revision": "2eabc2cc045b0d957f2d",
+    "url": "/react_app/static/js/runtime~main.05950baf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
